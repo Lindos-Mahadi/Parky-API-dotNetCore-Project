e634a35d0c6fbbef2ccaca1d92a076e5376c1750
Lindos-Mahadi <lindosmahadi@.com>
2021-12-04 19:11:19 +0600

    DataBase Connection SetUp & Installed 5 Packages

---
+++ ParkyAPI/Models/NationalPark.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    public class NationalPark
+    {
+
+    }
+}
