2918e9efbb213e1ef35b12a0f6a785e12f52115c
Lindos-Mahadi <lindosmahadi@.com>
2021-12-07 18:52:17 +0600

    Created GetUrlSD for Get API URL

---
+++ ParkyWebApp/GetUrlSD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWebApp
+{
+    public static class GetUrlSD
+    {
+        public static string APIBaseUrl = "https://localhost:22012/";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+    }
+}
